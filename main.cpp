f342c17f754cfc24de9ce48372c096d723efd4dd
tataglarasati11 <60909380+tataglarasati11@users.noreply.github.com>
2020-05-07 23:57:24 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,43 +6,42 @@
 using namespace std;
 
 void header(){
-	cout<<"==============================================="<<endl;
-	cout<<"menghitung Volume dan harga Kerucut dan Tabung "<<endl;
-	cout<<"==============================================="<<endl;
+   cout<<"==============================================="<<endl;
+   cout<<"menghitung Volume dan harga Kerucut dan Tabung "<<endl;
+   cout<<"==============================================="<<endl;
 }
 
 int main()
 {
-system("cls");
-tabung bung;
-kerucut cut;
+  system("cls");
+  tabung bung;
+  kerucut cut;
 
-bangun_ruang_sisi_lengkung *list[1];
-list[1]=&bung;
-bangun_ruang_sisi_lancip *poin[1];
-poin[1]=&cut;
+  bangun_ruang_sisi_lengkung *list[1];
+  list[1]=&bung;
+  bangun_ruang_sisi_lancip *poin[1];
+  poin[1]=&cut;
 
 	int pilih;
 	char ulang;
-		system("cls");
-		  header();
-		  cout<<"1) Tabung\n";
-		  cout<<"2) Kerucut\n";
-		  cout<<"masukkan pilihan:";
-		  cin>>pilih;
-		  
-		  if(pilih==1){
-		          list[1]->get_data();
-		          getch();
-		          }
-		  else if (pilih==2){
-		          poin[1]->get_data();
-		          getch();
-		          }
-		 else{
-		     cout <<"maaf pilihan yang anda masukkan salah"<<endl;
-		     getch();
-			}
+	system("cls");
+	header();
+	cout<<"1) Tabung\n";
+	cout<<"2) Kerucut\n";
+	cout<<"masukkan pilihan:";
+	cin>>pilih;	  
+	    if(pilih==1){
+		list[1]->get_data();
+	        getch();
+	        }
+	else if (pilih==2){
+	        poin[1]->get_data();
+	        getch();
+		}
+	 else{
+	        cout <<"maaf pilihan yang anda masukkan salah"<<endl;
+		getch();
+		}
     
 	system("cls");
 	cout << "Terima kasih telah menggunakan program saya untuk menghitung"<<endl<<endl;
@@ -50,5 +49,5 @@ poin[1]=&cut;
 	cout << "=============================="<<endl;	
 	cout << "Tatag Indah Larasati"<<endl;
 	cout << " D04217025"<<endl;
-    cout << "=============================="<<endl;	
+        cout << "=============================="<<endl;	
 }
